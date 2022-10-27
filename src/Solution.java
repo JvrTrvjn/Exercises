ce405611a32cde68a15c0659011a01d72cd05c0c
StWild <javier.trevijano@hotmail.com>
2022-10-27 11:17:45 +0200

    Second version

--- src/Solution.java
+++ src/Solution.java
@@ -11,18 +11,5 @@ public class Solution {
     public static void main(String[] args) {
         int[] myArrayTest = {6, -102, 1005, 200, 333, 8, -99, 0, 342};
         System.out.println(maxMultiplesOf3(myArrayTest));
-
-       /* Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-
-        in.nextLine();
-
-        for (int i = 1; i < N; i++){
-            for (int j = 1; j < N; j++){
-                System.out.print(i + " x " + j + " = " + (i * j));
-                System.out.println();
-            }
-        }
-            in.close();*/
     }
 }
