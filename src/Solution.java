45062c0fdd7aa543bc667fa4cf2de24356219aaa
StWild <javier.trevijano@hotmail.com>
2022-10-27 14:07:19 +0200

    Third Version

--- src/Solution.java
+++ src/Solution.java
@@ -1,15 +1,15 @@
 public class Solution {
-    public static int maxMultiplesOf3(int[] arr) {
-        int max = 0;
+    public static int maxMultiplesOf3(Integer[] arr) {
+       Integer max = arr[0];
 
-        for (int i : arr)
-            if (i % 3 == 0 && i > max)
-                max = i;
+        for (Integer numberArray : arr)
+            if (numberArray % 3 == 0 && numberArray > max)
+                max = numberArray;
         return max;
     }
 
     public static void main(String[] args) {
-        int[] myArrayTest = {6, -102, 1005, 200, 333, 8, -99, 0, 342};
+        Integer[] myArrayTest = {-100, -102, -1005, -200, -333, -8, -99, -0, -342};
         System.out.println(maxMultiplesOf3(myArrayTest));
     }
 }
