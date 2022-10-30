77790c18fa0bf8664d53d479f25a130d14a3f18b
StWild <javier.trevijano@hotmail.com>
2022-10-30 12:54:12 +0100

    Given an array find the maximum integer multiple of 3

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public static int maxMultiplesOf3(Integer[] arr) {
+    public static Integer maxMultiplesOf3(Integer[] arr) {
        Integer max = arr[0];
 
         for (Integer numberArray : arr)
@@ -7,9 +7,14 @@ public class Solution {
                 max = numberArray;
         return max;
     }
-
     public static void main(String[] args) {
-        Integer[] myArrayTest = {-100, -102, -200, 333, -8, -99, -30, -342, 300, 1011};
-        System.out.println(maxMultiplesOf3(myArrayTest));
+        Integer [] myArray = {4,7,11,9,8,21,2};
+        System.out.println(maxMultiplesOf3(myArray));
+
     }
-}
+
+
+        }
+
+
+
